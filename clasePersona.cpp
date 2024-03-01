37c269f400bca52b06f00afd5cc3a521bb8c7167
docenteTesteo24 <159298215+docenteTesteo24@users.noreply.github.com>
2024-03-01 13:30:16 -0400

    Add files via upload

---
+++ clasePersona.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Persona {
+	string nombre;
+public:
+	
+	Persona() : nombre("") {}
+	Persona(string n) : nombre(n) {}
+	string get_nombre(){ return nombre;}
+	void set_nombre(string n){ nombre=n;}
+};
+const int TAM=10;
+class GestorPersonas {
+private:
+	
+	Persona personas[TAM]; // Tamaño fijo para simplificar el ejemplo
+	int tamanoActual;
+	
+public:
+	GestorPersonas() : tamanoActual(0) {}
+	
+	void agregarPersona(const Persona& p) {
+		if(tamanoActual < 10) {
+			personas[tamanoActual++] = p;
+		} else {
+			cout << "Arreglo lleno." << endl;
+		}
+	}
+	
+	void imprimirPersonas() {
+		for(int i = 0; i < tamanoActual; ++i) {
+			cout << personas[i].get_nombre() << endl;
+		}
+	}
+	
+	int buscarPersona(const string& nombre) {
+		for(int i = 0; i < tamanoActual; ++i) {
+			if(personas[i].get_nombre() == nombre) {
+				return i;
+			}
+		}
+		return -1;
+	}
+};
+
+int main() {
+	GestorPersonas gestor;
+	Persona p;
+	p.set_nombre("Juan");
+	gestor.agregarPersona(p);
+	p.set_nombre("Ana");
+	
+	gestor.agregarPersona(p);
+	gestor.imprimirPersonas();
+	
+	string nombreABuscar = "Ana";
+	int indice = gestor.buscarPersona(nombreABuscar);
+	if(indice != -1) {
+		cout << "Persona encontrada en el índice " << indice << endl;
+	} else {
+		cout << "Persona no encontrada." << endl;
+	}
+	
+	return 0;
+}
